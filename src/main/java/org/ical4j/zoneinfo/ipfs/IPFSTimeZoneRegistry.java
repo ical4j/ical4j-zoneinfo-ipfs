68549a022b5f6ce1b844cb7206f0dab42af8a0bc
Ben Fortuna <bfortuna@odecee.com.au>
2022-04-05 15:00:03 +1000

    Added project skeleton

--- src/main/java/org/ical4j/zoneinfo/ipfs/IPFSTimeZoneRegistry.java
+++ src/main/java/org/ical4j/zoneinfo/ipfs/IPFSTimeZoneRegistry.java
@@ -1,9 +1,23 @@
 package org.ical4j.zoneinfo.ipfs;
 
+import io.ipfs.api.IPFS;
+import io.ipfs.multihash.Multihash;
 import net.fortuna.ical4j.model.TimeZone;
 import net.fortuna.ical4j.model.TimeZoneRegistry;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class IPFSTimeZoneRegistry implements TimeZoneRegistry {
+
+    private final IPFS ipfs;
+
+    private final String resourcePrefix;
+
+    public IPFSTimeZoneRegistry(IPFS ipfs, String resourcePrefix) {
+        this.ipfs = ipfs;
+        this.resourcePrefix = resourcePrefix;
+    }
 
     @Override
     public void register(TimeZone timezone) {
@@ -22,6 +36,13 @@ public class IPFSTimeZoneRegistry implements TimeZoneRegistry {
 
     @Override
     public TimeZone getTimeZone(String id) {
+        String hash = "QmT78zSuBmuS4z925WZfrqQ1qHaJ56DQaTfyMUF7F8ff5o"; // Hash of a file
+        Multihash multihash = Multihash.fromBase58(hash);
+        try {
+            byte[] bytes = ipfs.cat(multihash, id);
+        } catch (IOException e) {
+
+        }
         return null;
     }
 }
--- src/main/java/org/ical4j/zoneinfo/ipfs/IPFSTimeZoneRegistryFactory.java
+++ src/main/java/org/ical4j/zoneinfo/ipfs/IPFSTimeZoneRegistryFactory.java
@@ -1,5 +1,6 @@
 package org.ical4j.zoneinfo.ipfs;
 
+import io.ipfs.api.IPFS;
 import net.fortuna.ical4j.model.TimeZoneRegistry;
 import net.fortuna.ical4j.model.TimeZoneRegistryFactory;
 
@@ -7,6 +8,6 @@ public class IPFSTimeZoneRegistryFactory extends TimeZoneRegistryFactory {
 
     @Override
     public TimeZoneRegistry createRegistry() {
-        return new IPFSTimeZoneRegistry();
+        return new IPFSTimeZoneRegistry(new IPFS("localhost", 5001), "zoneinfo/");
     }
 }
